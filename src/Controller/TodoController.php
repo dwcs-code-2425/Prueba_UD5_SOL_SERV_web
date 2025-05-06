15df52ce11fd569d0d8513a7cb894b064fcad88d
dwcs-code-2425 <dwcs.code.2425@gmail.com>
2025-05-06 18:34:54 +0200

    ruta global para todos métodos y serializer

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\TodoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route("/api", "api_")]
 final class TodoController extends AbstractController
 {
-    #[Route('/todo', name: 'app_todo')]
-    public function index(): JsonResponse
+
+    //https://symfony.com/doc/current/serializer.html
+    #[Route('/todos', name: 'todos', methods: ["GET"])]
+    public function index(TodoRepository $todoRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TodoController.php',
-        ]);
+        $todos = $todoRepository->findAll();
+
+        return $this->json($todos);
     }
 }
